81c47708908562048952666a0245a6eecadc166c
Cooper Eisman <76635972+CooperEisman@users.noreply.github.com>
2023-07-02 15:31:54 -0600

    Added Persistence Management

---
+++ src/Filament.java
@@ -0,0 +1,15 @@
+// Created by Cooper Eisman on 30JUN2023 @ 2300 MT
+
+public class Filament {
+    private String brand;           //
+    private String material;        //
+    private String color;           //
+
+    private double diameter;        // Measured in mm
+    private double density;         // Measured in g/cm3
+    private double tensileStrength; // Measured in MPa
+
+    public Filament() {}
+
+
+}
